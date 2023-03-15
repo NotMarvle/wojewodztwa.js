0d5d3f56ebc7c299ee32d88c69d17b4999529448
NotMarvle <puffythereall@gmail.com>
2023-03-15 07:40:10 +0100

    Added Województwa

---
+++ README.md
@@ -0,0 +1,4 @@
+# Wojewodztwa.js
+
+> #### About
+> #### This is a simple quiz website that i made for my school.
---
+++ Tryb/app.js
@@ -0,0 +1,9 @@
+const wojewodztwa = document.getElementById("wojewodztwa");
+const stolice = document.getElementById("stolice");
+
+stolice.onclick = function() {
+    location.href = "Stolice";
+}
+wojewodztwa.onclick = function() {
+    location.href = "Wojewodztwa";
+}
